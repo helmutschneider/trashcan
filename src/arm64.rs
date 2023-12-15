0e5bbd70dab592a37ff6302f60553dce62835017
Johan Björk <johanimon@gmail.com>
2023-12-15 20:46:21 +0100

    Add rare docs for the 'eq' flag

--- src/arm64.rs
+++ src/arm64.rs
@@ -3,9 +3,9 @@ use crate::bytecode::Bytecode;
 use crate::bytecode::Const;
 use crate::bytecode::Instruction;
 use crate::bytecode::ENTRYPOINT_NAME;
+use crate::util::determine_stack_size_of_function;
 use crate::util::Env;
 use crate::util::Error;
-use crate::util::determine_stack_size_of_function;
 use std::rc::Rc;
 
 macro_rules! emit {
@@ -196,6 +196,8 @@ impl<'a> ARM64Assembly<'a> {
                 Instruction::IsEqual(reg, r1) => {
                     let reg: Register = reg.into();
                     let r1: Register = r1.into();
+
+                    // https://developer.arm.com/documentation/den0042/a/Unified-Assembly-Language-Instructions/Instruction-set-basics/Conditional-execution?lang=en#CHDBEIHD
                     emit!(self, "  cmp {}, {}", reg, r1);
                     emit!(self, "  cset {}, eq", reg);
                 }
@@ -239,7 +241,10 @@ impl<'a> ARM64Assembly<'a> {
                 Instruction::Return => {
                     if fx_name == ENTRYPOINT_NAME {
                         // do an implicit exit syscall.
-                        self.emit_mov_immediate(Register(self.env.syscall_register), self.env.syscall_exit);
+                        self.emit_mov_immediate(
+                            Register(self.env.syscall_register),
+                            self.env.syscall_exit,
+                        );
                         emit!(self, "  mov x0, #0");
                         emit!(self, "  svc #0");
                     }
